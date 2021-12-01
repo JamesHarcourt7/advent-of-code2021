f109f9132757539f00ce684303bd2b7e0a209ac4
James Harcourt <jh3g20@soton.ac.uk>
2021-12-01 22:59:34 +0000

    Completed day 1.

--- day01.hs
+++ day01.hs
@@ -8,12 +8,25 @@ main = do
     contents <- hGetContents handle
     let inputs = lines contents
         answer1 = partOne inputs
+        answer2 = partTwo inputs
     
     putStr ("Part one: " ++ (show answer1) ++ "\n")
+    putStr ("Part two: " ++ (show answer2) ++ "\n")
     hClose handle
 
 aux :: String -> String -> Bool
 aux x y = (read y) - (read x) > 0
 
+sumWindow :: (String, String, String) -> Int
+sumWindow (x, y, z) = (read x) + (read y) + (read z)
+
+aux2 :: Int -> Int -> Bool
+aux2 x y = y - x > 0
+
 partOne :: [String] -> Int
 partOne inputs = length (filter (\x -> x) (zipWith aux inputs (tail inputs)))
+
+partTwo :: [String] -> Int
+partTwo inputs = length (filter (\x -> x) (zipWith aux2 summed (tail summed)))
+    where triples = zip3 inputs (tail inputs) (tail (tail inputs))
+          summed = (map (sumWindow) triples)
